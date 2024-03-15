fab27f6832a5f97b83bd1cfc1dddb485ade322fe
ddigitech <ddigitech@gmail.com>
2024-03-14 21:18:48 -0400

    03142024-evening-1

--- server/database/app.js
+++ server/database/app.js
@@ -71,6 +71,12 @@ try {
 // Express route to fetch Dealers by a particular state
 app.get('/fetchDealers/:state', async (req, res) => {
 //Write your code here
+try {
+    const documents = await Dealerships.find({dealership: req.params.state});
+    res.json(documents);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching documents' });
+  }
 });
 
 // Express route to fetch dealer by a particular id
