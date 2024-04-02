9d5f5b733bf38e02de75b3a49982838c09b804bf
ddigitech <ddigitech@gmail.com>
2024-04-02 18:46:14 -0500

    Update app.js

--- server/database/app.js
+++ server/database/app.js
@@ -104,11 +104,11 @@ app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
 		"name": data.name,
 		"dealership": data.dealership,
 		"review": data.review,
-		"purchase": data['purchase'],
-		"purchase_date": data['purchase_date'],
-		"car_make": data['car_make'],
-		"car_model": data['car_model'],
-		"car_year": data['car_year'],
+		"purchase": data.purchase,
+		"purchase_date": data.purchase_date,
+		"car_make": data.car_make,
+		"car_model": data.car_model,
+		"car_year": data.car_year,
 	});
 
   try {
